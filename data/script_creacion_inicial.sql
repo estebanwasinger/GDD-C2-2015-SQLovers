4727a9f5c89085917ba2191120a2af51514b02b8
Esteban Wasinger <estebanwasinger@gmail.com>
2015-10-04 15:45:10 -0300

    * A todos los clientes se le crea un usuario y contraseña

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -8,8 +8,8 @@ IF NOT EXISTS (SELECT schema_name
         N'CREATE SCHEMA SQLOVERS' 
   END 
 
-/*   
-DROP ALL THE TABLES!!!   
+/*    
+DROP ALL THE TABLES!!!
 */ 
 IF Object_id('SQLOVERS.Pasaje') IS NOT NULL 
   BEGIN 
@@ -21,19 +21,20 @@ IF Object_id('SQLOVERS.Usuario') IS NOT NULL
       DROP TABLE sqlovers.usuario; 
   END; 
 
-/*   
-CREATE TABLES   
+/*    
+CREATE TABLES
 */ 
 CREATE TABLE sqlovers.usuario 
   ( 
      cli_nombre    NVARCHAR(255), 
      cli_apellido  NVARCHAR(255), 
-     cli_dni       NUMERIC(18, 0), 
+     cli_dni       NUMERIC(18, 0) NOT NULL PRIMARY KEY, 
      cli_dir       NVARCHAR(255), 
      cli_telefono  NUMERIC(18, 0), 
      cli_mail      NVARCHAR(255), 
      cli_fecha_nac DATETIME, 
-     CONSTRAINT pk_usuario PRIMARY KEY (cli_dni) 
+     cli_usuario   NVARCHAR(255), 
+     cli_password  VARCHAR(255) 
   ); 
 
 CREATE TABLE sqlovers.pasaje 
@@ -45,8 +46,8 @@ CREATE TABLE sqlovers.pasaje
      sqlovers.usuario(cli_dni) 
   ) 
 
-/*  
-FILL TABLES  
+/*   
+FILL TABLES   
 */ 
 INSERT INTO sqlovers.usuario 
             (cli_nombre, 
@@ -55,14 +56,19 @@ INSERT INTO sqlovers.usuario
              cli_dir, 
              cli_telefono, 
              cli_mail, 
-             cli_fecha_nac) 
+             cli_fecha_nac, 
+             cli_usuario, 
+             cli_password) 
 SELECT cli_nombre, 
        cli_apellido, 
        cli_dni, 
        cli_dir, 
        cli_telefono, 
        cli_mail, 
-       cli_fecha_nac 
+       cli_fecha_nac, 
+       Lower(Replace(cli_nombre, ' ', '.') + '.' 
+             + Replace(cli_apellido, ' ', '.')), 
+       Hashbytes('SHA2_256', CONVERT(VARCHAR(20), cli_dni)) 
 FROM   (SELECT cli_nombre, 
                cli_apellido, 
                cli_dni, 
@@ -76,6 +82,14 @@ FROM   (SELECT cli_nombre,
                    ORDER BY cli_dni) AS RowNumber 
         FROM   gd_esquema.maestra) AS a 
 WHERE  a.rownumber = 1 
+
+INSERT INTO sqlovers.usuario 
+            (cli_dni, 
+             cli_usuario, 
+             cli_password) 
+VALUES      (00000000, 
+             'admin', 
+             'e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7') 
 
 INSERT INTO sqlovers.pasaje 
             (pasaje_codigo, 
