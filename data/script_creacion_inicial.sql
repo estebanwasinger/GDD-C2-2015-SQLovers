b0f3cae99debd074a22235c6f61c5966b7942dd8
estebanwasinger <estebanwasinger@gmail.com>
2015-10-10 21:38:27 -0300

    * Se separó la tabla de Usuario de los clients * Se creo la tabla Ciudad y Ruta (Con sus correspondientes insersiones)

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -9,11 +9,16 @@ IF NOT EXISTS (SELECT schema_name
   END 
 
 /*    
-DROP ALL THE TABLES!!!
+DROP ALL THE TABLES!!!    
 */ 
 IF Object_id('SQLOVERS.Pasaje') IS NOT NULL 
   BEGIN 
       DROP TABLE sqlovers.pasaje; 
+  END; 
+
+IF Object_id('SQLOVERS.Cliente') IS NOT NULL 
+  BEGIN 
+      DROP TABLE sqlovers.cliente; 
   END; 
 
 IF Object_id('SQLOVERS.Usuario') IS NOT NULL 
@@ -21,10 +26,35 @@ IF Object_id('SQLOVERS.Usuario') IS NOT NULL
       DROP TABLE sqlovers.usuario; 
   END; 
 
+IF Object_id('SQLOVERS.Ciudad') IS NOT NULL 
+  BEGIN 
+      DROP TABLE sqlovers.ciudad; 
+  END; 
+
+IF Object_id('SQLOVERS.Ruta') IS NOT NULL 
+  BEGIN 
+      DROP TABLE sqlovers.ruta; 
+  END; 
+
 /*    
-CREATE TABLES
+CREATE TABLES    
 */ 
+CREATE TABLE sqlovers.ciudad 
+  ( 
+     ciudad_id     NUMERIC(6, 0) IDENTITY NOT NULL PRIMARY KEY, 
+     ciudad_nombre NVARCHAR(255) 
+  ) 
+
 CREATE TABLE sqlovers.usuario 
+  ( 
+     user_username     NVARCHAR(255) NOT NULL PRIMARY KEY, 
+     user_password     NVARCHAR(255), 
+     user_nro_intentos NUMERIC(18, 0), 
+     user_estado       NUMERIC(1, 0), 
+     user_rol_id       NUMERIC(18, 0) 
+  ); 
+
+CREATE TABLE sqlovers.cliente 
   ( 
      cli_nombre    NVARCHAR(255), 
      cli_apellido  NVARCHAR(255), 
@@ -33,8 +63,8 @@ CREATE TABLE sqlovers.usuario
      cli_telefono  NUMERIC(18, 0), 
      cli_mail      NVARCHAR(255), 
      cli_fecha_nac DATETIME, 
-     cli_usuario   NVARCHAR(255), 
-     cli_password  VARCHAR(255) 
+     cli_username  NVARCHAR(255) FOREIGN KEY REFERENCES 
+     sqlovers.usuario(user_username) 
   ); 
 
 CREATE TABLE sqlovers.pasaje 
@@ -43,32 +73,64 @@ CREATE TABLE sqlovers.pasaje
      pasaje_precio      NUMERIC(18, 2), 
      pasaje_fechacompra DATETIME, 
      cli_dni            NUMERIC(18, 0) FOREIGN KEY REFERENCES 
-     sqlovers.usuario(cli_dni) 
+     sqlovers.cliente(cli_dni) 
+  ) 
+
+CREATE TABLE sqlovers.ruta 
+  ( 
+     ruta_id                NUMERIC(18, 0) NOT NULL IDENTITY PRIMARY KEY, 
+     ruta_ciudad_origen     NVARCHAR(255), 
+     ruta_ciudad_destino    NVARCHAR(255), 
+     ruta_tipo_servicio     NVARCHAR(255), 
+     ruta_precio_basepasaje NUMERIC(18, 0), 
+     ruta_precio_basekg     NUMERIC(18, 0) 
   ) 
 
 /*   
 FILL TABLES   
 */ 
+INSERT INTO sqlovers.ruta 
+            (ruta_precio_basekg, 
+             ruta_ciudad_destino, 
+             ruta_ciudad_origen, 
+             ruta_precio_basepasaje, 
+             ruta_tipo_servicio) 
+SELECT DISTINCT (SELECT Max(ruta_precio_basekg) 
+                 FROM   gd_esquema.maestra m2 
+                 WHERE  m2.ruta_ciudad_destino = m1.ruta_ciudad_destino 
+                        AND m2.ruta_ciudad_origen = m1.ruta_ciudad_origen 
+                        AND m2.tipo_servicio = m1.tipo_servicio) AS 
+                Ruta_Precio_BaseKG, 
+                ruta_ciudad_destino, 
+                ruta_ciudad_origen, 
+                (SELECT Max(ruta_precio_basepasaje) 
+                 FROM   gd_esquema.maestra m2 
+                 WHERE  m2.ruta_ciudad_destino = m1.ruta_ciudad_destino 
+                        AND m2.ruta_ciudad_origen = m1.ruta_ciudad_origen 
+                        AND m2.tipo_servicio = m1.tipo_servicio) AS 
+                Ruta_Precio_BasePasaje, 
+                tipo_servicio 
+FROM   [GD2C2015].[gd_esquema].[maestra] m1 
+WHERE  m1.ruta_ciudad_destino != m1.ruta_ciudad_origen 
+
+INSERT INTO sqlovers.ciudad 
+            (ciudad_nombre) 
+SELECT DISTINCT ruta_ciudad_origen 
+FROM   gd_esquema.maestra 
+
 INSERT INTO sqlovers.usuario 
-            (cli_nombre, 
-             cli_apellido, 
-             cli_dni, 
-             cli_dir, 
-             cli_telefono, 
-             cli_mail, 
-             cli_fecha_nac, 
-             cli_usuario, 
-             cli_password) 
-SELECT cli_nombre, 
-       cli_apellido, 
-       cli_dni, 
-       cli_dir, 
-       cli_telefono, 
-       cli_mail, 
-       cli_fecha_nac, 
-       Lower(Replace(cli_nombre, ' ', '.') + '.' 
-             + Replace(cli_apellido, ' ', '.')), 
-       Hashbytes('SHA2_256', CONVERT(VARCHAR(20), cli_dni)) 
+            (user_username, 
+             user_password, 
+             user_nro_intentos, 
+             user_estado, 
+             user_rol_id) 
+SELECT Lower(Replace(cli_nombre, ' ', '.') + '.' 
+             + Replace(cli_apellido, ' ', '.') 
+             + CONVERT(VARCHAR(20), cli_dni)), 
+       Hashbytes('SHA2_256', CONVERT(VARCHAR(20), cli_dni)), 
+       0, 
+       1, 
+       NULL 
 FROM   (SELECT cli_nombre, 
                cli_apellido, 
                cli_dni, 
@@ -83,14 +145,41 @@ FROM   (SELECT cli_nombre,
         FROM   gd_esquema.maestra) AS a 
 WHERE  a.rownumber = 1 
 
-INSERT INTO sqlovers.usuario 
-            (cli_dni, 
-             cli_usuario, 
-             cli_password) 
-VALUES      (00000000, 
-             'admin', 
-             'e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7') 
+INSERT INTO sqlovers.cliente 
+            (cli_nombre, 
+             cli_apellido, 
+             cli_dni, 
+             cli_dir, 
+             cli_telefono, 
+             cli_mail, 
+             cli_fecha_nac, 
+             cli_username) 
+SELECT cli_nombre, 
+       cli_apellido, 
+       cli_dni, 
+       cli_dir, 
+       cli_telefono, 
+       cli_mail, 
+       cli_fecha_nac, 
+       Lower(Replace(cli_nombre, ' ', '.') + '.' 
+             + Replace(cli_apellido, ' ', '.') 
+             + CONVERT(VARCHAR(20), cli_dni)) 
+FROM   (SELECT cli_nombre, 
+               cli_apellido, 
+               cli_dni, 
+               cli_dir, 
+               cli_telefono, 
+               cli_mail, 
+               cli_fecha_nac, 
+               Row_number() 
+                 OVER ( 
+                   partition BY cli_dni 
+                   ORDER BY cli_dni) AS RowNumber 
+        FROM   gd_esquema.maestra) AS a 
+WHERE  a.rownumber = 1 
 
+--INSERT INTO sqlovers.usuario(cli_dni, cli_usuario, cli_password) 
+--VALUES (00000000, 'admin','e6b87050bfcb8143fcb8db0170a4dc9ed00d904ddd3e2a4ad1b1e8dc0fdc9be7') 
 INSERT INTO sqlovers.pasaje 
             (pasaje_codigo, 
              pasaje_precio, 
