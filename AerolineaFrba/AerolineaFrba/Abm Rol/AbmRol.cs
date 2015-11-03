7d52c5069c01f4f0d3a3c31f4b8ee100ab203bf0
Laura Sasal <LauraSasal@users.noreply.github.com>
2015-11-03 17:58:35 -0300

    Update AbmRol.cs

--- AerolineaFrba/AerolineaFrba/Abm Rol/AbmRol.cs
+++ AerolineaFrba/AerolineaFrba/Abm Rol/AbmRol.cs
@@ -1,22 +1,102 @@
+using AerolineaFrba.Models.BO;
+using AerolineaFrba.Models.DAO;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AerolineaFrba.Abm_Rol
 {
-    static class Program
+    public partial class ABMRol : Form
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private List<Rol> rolList;
+        private DAORol daoRol;
+        private Rol selectedRol;
+
+        public ABMRol()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Listado());
+            InitializeComponent();
+            rolList = new List<Rol>();
+            daoRol = new DAORol();
         }
+
+        private void ABMRol_Load(object sender, EventArgs e)
+        {
+            dataGridViewRol.AutoGenerateColumns = false;
+            dataGridViewRol.MultiSelect = false;
+            modificarButton.Enabled = false;
+            borrarButton.Enabled = false;
+            createColumns();
+            fillDataGridView();
+        }
+
+        private void fillDataGridView()
+        {
+            rolList = daoRol.retrieveAll();
+
+            dataGridViewRol.DataSource = rolList;
+        }
+
+        private void createColumns()
+        {
+            DataGridViewTextBoxColumn colId = new DataGridViewTextBoxColumn();
+            colId.DataPropertyName = "id";
+            colId.HeaderText = "Id";
+            colId.Width = 70;
+            colId.ReadOnly = true;
+
+            DataGridViewTextBoxColumn colNombre = new DataGridViewTextBoxColumn();
+            colNombre.DataPropertyName = "name";
+            colNombre.HeaderText = "Nombre";
+            colNombre.Width = 120;
+            colNombre.ReadOnly = true;
+
+            DataGridViewTextBoxColumn colEstado = new DataGridViewTextBoxColumn();
+            colEstado.DataPropertyName = "activo";
+            colEstado.HeaderText = "Estado";
+            colEstado.Width = 50;
+            colEstado.ReadOnly = true;
+
+            dataGridViewRol.Columns.Add(colId);
+            dataGridViewRol.Columns.Add(colNombre);
+            dataGridViewRol.Columns.Add(colEstado);
+            dataGridViewRol.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void dataGridViewRol_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            borrarButton.Enabled = true;
+            modificarButton.Enabled = true;
+        }
+
+        private void borrarButton_Click(object sender, EventArgs e)
+        {
+            selectedRol = (Rol)dataGridViewRol.CurrentRow.DataBoundItem;
+            System.Windows.Forms.MessageBox.Show("Rol Nombre: " + selectedRol.name + " Rol Id: " + selectedRol.id);
+            daoRol.deleteRol((int) selectedRol.id);
+            fillDataGridView();
+        }
+
+        private void modificarButton_Click(object sender, EventArgs e)
+        {
+            selectedRol = (Rol)dataGridViewRol.CurrentRow.DataBoundItem;
+            UpdateOrCreateView view = new UpdateOrCreateView(selectedRol, daoRol, false);
+            view.ShowDialog();
+            fillDataGridView();
+        }
+
+        private void crearButton_Click(object sender, EventArgs e)
+        {
+            Rol newRol = new Rol();
+            UpdateOrCreateView view = new UpdateOrCreateView(newRol, daoRol, true);
+            view.ShowDialog();
+            fillDataGridView();
+        }
+
     }
 }
