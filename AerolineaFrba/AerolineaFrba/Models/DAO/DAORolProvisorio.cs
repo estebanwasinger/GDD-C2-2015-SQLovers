1a297a22b26c550fe06b3cda026f50bc7f7417f6
Laura Sasal <LauraSasal@users.noreply.github.com>
2015-11-03 19:25:08 -0300

    Create DAORolProvisorio.cs

---
+++ AerolineaFrba/AerolineaFrba/Models/DAO/DAORolProvisorio.cs
@@ -0,0 +1,80 @@
+using AerolineaFrba.Models.BO;
+using AerolineaFrba.Models.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerolineaFrba.Models.DAO
+{
+    public partial class DAORol : DAOBase<Rol>
+    {
+
+        public DAORol()
+            : base("SQLOVERS.ROL", "rol_id")
+        { }
+
+        public int deleteRol(int key)
+        {
+            List<SqlParameter> listaParametros = new List<SqlParameter>();
+            listaParametros.Add(new SqlParameter("@rol_id", key));
+            listaParametros.Add(new SqlParameter("@rol_activo", false));
+
+            return Convert.ToInt32(DBAcess.WriteInBase("update SQLOVERS.rol set rol_activo=@rol_activo where rol_id=@rol_id", "T", listaParametros));
+        }
+
+        public bool create(Rol rol)
+        {
+            try
+            {
+                List<SqlParameter> ListaParametros = new List<SqlParameter>();
+
+                ListaParametros.Add(new SqlParameter("@rol_name", rol.name));
+                ListaParametros.Add(new SqlParameter("@rol_activo", rol.activo));
+
+                return DBAcess.WriteInBase("insert into SQLOVERS.rol (rol_name, rol_activo) VALUES(@rol_name, @rol_activo)", "T", ListaParametros);
+            }
+            catch { return false; }
+        }
+
+        public bool update(Rol rol)
+        {
+            try
+            {
+                List<SqlParameter> ListaParametros = new List<SqlParameter>();
+
+                ListaParametros.Add(new SqlParameter("@rol_id", rol.id));
+                ListaParametros.Add(new SqlParameter("@rol_name", rol.name));
+                ListaParametros.Add(new SqlParameter("@rol_activo", rol.activo));
+
+                return DBAcess.WriteInBase("update SQLOVERS.rol set rol_name=@rol_name, rol_activo=@rol_activo where rol_id = @rol_id ", "T", ListaParametros);
+            }
+            catch { return false; }
+        }
+
+
+        public List<Rol> retrieveAll()
+        {
+            List<Rol> rolList = new List<Rol>();
+            SqlDataReader lector = DBAcess.GetDataReader("SELECT rol_id,rol_name,CASE WHEN rol_activo = 1 THEN '1' ELSE '0' END AS rol_activo from SQLOVERS.ROL", "T", new List<SqlParameter>());
+
+            if (lector.HasRows)
+            {
+                while (lector.Read())
+                {
+                    Rol rol = new Rol();
+                    rol.name = (string)lector["rol_name"];
+                    rol.id = (int)(decimal)lector["rol_id"];
+                    rol.activo = lector["rol_activo"].ToString() == "1";
+                    //rol.activo = (lector["rol_activo"] as bool?) ?? false;
+
+                    rolList.Add(rol);
+                }
+            }
+            return rolList;
+        }
+
+    }
+}
