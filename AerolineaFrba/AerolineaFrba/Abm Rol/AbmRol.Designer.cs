a4d1242645a70fff0d985c9d8da64abc3418c3bb
Laura Sasal <LauraSasal@users.noreply.github.com>
2015-11-11 22:24:48 -0300

    Update AbmRol.Designer.cs

--- AerolineaFrba/AerolineaFrba/Abm Rol/AbmRol.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Abm Rol/AbmRol.Designer.cs
@@ -29,72 +29,62 @@ namespace AerolineaFrba.Abm_Rol
         private void InitializeComponent()
         {
             this.dataGridViewRol = new System.Windows.Forms.DataGridView();
-            this.crearButton = new System.Windows.Forms.Button();
-            this.modificarButton = new System.Windows.Forms.Button();
-            this.borrarButton = new System.Windows.Forms.Button();
+            this.btnCrearRol = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRol)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewRol
             // 
             this.dataGridViewRol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewRol.Location = new System.Drawing.Point(12, 78);
+            this.dataGridViewRol.Location = new System.Drawing.Point(14, 64);
             this.dataGridViewRol.Name = "dataGridViewRol";
-            this.dataGridViewRol.Size = new System.Drawing.Size(312, 263);
+            this.dataGridViewRol.Size = new System.Drawing.Size(537, 213);
             this.dataGridViewRol.TabIndex = 0;
-            this.dataGridViewRol.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRol_CellClick);
+            this.dataGridViewRol.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRol_CellContentClick);
             // 
-            // crearButton
+            // btnCrearRol
             // 
-            this.crearButton.Location = new System.Drawing.Point(12, 12);
-            this.crearButton.Name = "crearButton";
-            this.crearButton.Size = new System.Drawing.Size(100, 60);
-            this.crearButton.TabIndex = 1;
-            this.crearButton.Text = "Crear";
-            this.crearButton.UseVisualStyleBackColor = true;
-            this.crearButton.Click += new System.EventHandler(this.crearButton_Click);
+            this.btnCrearRol.Location = new System.Drawing.Point(259, 304);
+            this.btnCrearRol.Name = "btnCrearRol";
+            this.btnCrearRol.Size = new System.Drawing.Size(75, 23);
+            this.btnCrearRol.TabIndex = 4;
+            this.btnCrearRol.Text = "Crear Rol";
+            this.btnCrearRol.UseVisualStyleBackColor = true;
+            this.btnCrearRol.Click += new System.EventHandler(this.btnCrearRol_Click);
             // 
-            // modificarButton
+            // label1
             // 
-            this.modificarButton.Location = new System.Drawing.Point(119, 12);
-            this.modificarButton.Name = "modificarButton";
-            this.modificarButton.Size = new System.Drawing.Size(100, 60);
-            this.modificarButton.TabIndex = 2;
-            this.modificarButton.Text = "Modificar";
-            this.modificarButton.UseVisualStyleBackColor = true;
-            this.modificarButton.Click += new System.EventHandler(this.modificarButton_Click);
-            // 
-            // borrarButton
-            // 
-            this.borrarButton.Location = new System.Drawing.Point(224, 12);
-            this.borrarButton.Name = "borrarButton";
-            this.borrarButton.Size = new System.Drawing.Size(100, 60);
-            this.borrarButton.TabIndex = 3;
-            this.borrarButton.Text = "Borrar";
-            this.borrarButton.UseVisualStyleBackColor = true;
-            this.borrarButton.Click += new System.EventHandler(this.borrarButton_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(191, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Listado de Roles";
 
             // 
             // ABMRol
             // 
-            this.ClientSize = new System.Drawing.Size(338, 353);
-            this.Controls.Add(this.borrarButton);
-            this.Controls.Add(this.modificarButton);
-            this.Controls.Add(this.crearButton);
+            this.ClientSize = new System.Drawing.Size(563, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCrearRol);
             this.Controls.Add(this.dataGridViewRol);
             this.Name = "ABMRol";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Load += new System.EventHandler(this.ABMRol_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRol)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
         
         private System.Windows.Forms.DataGridView dataGridViewRol;
-        private System.Windows.Forms.Button crearButton;
-        private System.Windows.Forms.Button modificarButton;
-        private System.Windows.Forms.Button borrarButton;
+        private System.Windows.Forms.Button btnCrearRol;
+        private System.Windows.Forms.Label label1;
+
+       
 
     }
 }
