4e1616e0db419dec296d5ba623820535448e4687
ovilcas <oscar.vilcas@gmail.com>
2015-12-04 14:43:33 -0800

    avanzo con la devolucion

--- AerolineaFrba/AerolineaFrba/Devolucion/Form1.cs
+++ AerolineaFrba/AerolineaFrba/Devolucion/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AerolineaFrba.Devolucion
 {
-    public partial class Form1 : Form
+    public partial class CancelarViaje : Form
     {
-        public Form1()
+        public CancelarViaje()
         {
             InitializeComponent();
         }
