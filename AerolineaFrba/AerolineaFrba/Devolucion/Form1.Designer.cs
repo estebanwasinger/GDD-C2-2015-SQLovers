13ca5efd3d1d648f0bffad00a68fb19b6f78598e
ovilcas <oscar.vilcas@gmail.com>
2015-12-07 10:13:19 -0800

    cambios a devolucion

--- AerolineaFrba/AerolineaFrba/Devolucion/Form1.Designer.cs
+++ AerolineaFrba/AerolineaFrba/Devolucion/Form1.Designer.cs
@@ -31,10 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dtgProdCancelar = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProdCancelar)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -62,13 +62,13 @@
             this.button1.Text = "Buscar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dtgProdCancelar
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 72);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(602, 229);
-            this.dataGridView1.TabIndex = 3;
+            this.dtgProdCancelar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgProdCancelar.Location = new System.Drawing.Point(40, 72);
+            this.dtgProdCancelar.Name = "dtgProdCancelar";
+            this.dtgProdCancelar.Size = new System.Drawing.Size(602, 229);
+            this.dtgProdCancelar.TabIndex = 3;
             // 
             // button2
             // 
@@ -95,13 +95,13 @@
             this.ClientSize = new System.Drawing.Size(687, 369);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dtgProdCancelar);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "CancelarViaje";
             this.Text = "CancelarViaje";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgProdCancelar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,7 +112,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtgProdCancelar;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button btnSalir;
     }
--- AerolineaFrba/AerolineaFrba/Devolucion/Form1.cs
+++ AerolineaFrba/AerolineaFrba/Devolucion/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AerolineaFrba.Models.BO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,45 @@ namespace AerolineaFrba.Devolucion
         {
             InitializeComponent();
         }
+
+        private List<Pasaje> lstPasaje { get; set; }
+
+        private void FormCancelarViaje_Load(object sender, EventArgs e) {
+
+            dtgProdCancelar.AutoGenerateColumns = false;
+            dtgProdCancelar.MultiSelect = false;
+
+            cargarGrilla();
+            actualizarGrilla();
+        }
+
+
+        private void cargarGrilla()
+        {
+            DataGridViewTextBoxColumn ColCodigo = new DataGridViewTextBoxColumn();
+            ColCodigo.DataPropertyName = "matricula";
+            ColCodigo.HeaderText = "Matricula";
+            ColCodigo.Width = 120;
+
+            DataGridViewTextBoxColumn ColFechaCompra = new DataGridViewTextBoxColumn();
+            ColFechaCompra.DataPropertyName = "descripcion_tipoServicio";
+            ColFechaCompra.HeaderText = "Servicio";
+            ColFechaCompra.Width = 120;
+
+            dtgProdCancelar.Columns.Add(ColCodigo);
+            dtgProdCancelar.Columns.Add(ColFechaCompra);
+
+            
+        }
+
+
+        public void actualizarGrilla()
+        {
+
+            lstPasaje = daoPasaje.aeronave_servicio();
+            dtgAeronavesPosibles.DataSource = lstAeronaves;
+
+        }
+
     }
 }
